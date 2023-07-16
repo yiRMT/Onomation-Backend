2b7988830f9bbf007c860a1a84641ca86d0eeecc
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2023-07-16 09:14:11 +0900

    プロンプトを変更

--- app.py
+++ app.py
@@ -63,24 +63,43 @@ async def gpt(text: str) -> GPTResponseModel:
 
 
 def requst_gpt(text):
-    example_html = ('<!DOCTYPE html><html><head><title>ザーザー Animation</title><link rel="stylesheet" type="text/css"></head><body><div id="container"><div id="circle"></div></div><script src="script.js"></script></body></html>')
-    example_css = ('#container { position: relative; width: 200px; height: 200px; overflow: hidden; } #circle { position: absolute; width: 100%; height: 100%; border-radius: 50%; background-color: blue; } ')
-    example_javascript = ('const circle = document.getElementById("circle"); function animateCircle() { circle.style.transform = "scale(2)"; circle.style.transition = "transform 0.5s ease-in-out"; setTimeout(function() { circle.style.transform = "scale(1)"; circle.style.transition = "transform 0.5s ease-in-out"; }, 500); setTimeout(animateCircle, 1000); } animateCircle(); ')
-    example_output = '{ "html": "' + example_html + '", "css": "' + example_css + '", "javascript": "' + example_javascript + '" }'
+    example_html_1 = ('<!DOCTYPE html><html><head><title>ザーザー雨のアニメーション</title></head><body><div id="gzxbbjuyxj"><div class="drop"></div></div></body></html>')
+    example_css_1 = ('body { margin: 0; padding: 0; overflow: hidden; } #gzxbbjuyxj { width: 100%; height: 100vh; position: absolute; top: 0; left: 0; pointer-events: none; } .drop { position: absolute; width: 2px; height: 120px; background-color: #0ff; opacity: 0.8; animation: dropFall linear infinite; } @keyframes dropFall { 0% { transform: translateY(-100%) rotate(45deg); } 50% { transform: translateY(100vh) rotate(45deg); } 100% { transform: translateY(100vh) rotate(-45deg); } }')
+    example_javascript_1 = ('const numDrops = 150; for (let i = 0; i < numDrops; i++) { createDrop(); } function createDrop() { const drop = document.createElement("div"); drop.className = "drop"; drop.style.left = Math.random() * 100 + "%"; drop.style.animationDuration = Math.random() * 2 + 1 + "s"; drop.style.animationDelay = Math.random() * 2 + "s"; document.getElementById("rainContainer").appendChild(drop); }')
+    example_output_1 = '{ "html": "' + example_html_1 + '", "css": "' + example_css_1 + '", "javascript": "' + example_javascript_1 + '" }'
+    
+    example_html_2 = ('<div id="mjelrdezft"></div>')
+    example_css_2 = ('body { margin: 0; padding: 0; overflow: hidden; } #rainContainer { width: 100%; height: 100vh; position: absolute; top: 0; left: 0; pointer-events: none; } .drop { position: absolute; width: 2px; height: 120px; background-color: #0ff; opacity: 0.8; animation: dropFall linear infinite; } @keyframes dropFall { 0% { transform: translateY(-100%) rotate(45deg); } 50% { transform: translateY(100vh) rotate(45deg); } 100% { transform: translateY(100vh) rotate(-45deg); } }')
+    example_javascript_2 = ('const numDrops = 150; for (let i = 0; i < numDrops; i++) { createDrop(); } function createDrop() { const drop = document.createElement("div"); drop.className = "drop"; drop.style.left = Math.random() * 100 + "%"; drop.style.animationDuration = Math.random() * 2 + 1 + "s"; drop.style.animationDelay = Math.random() * 2 + "s"; document.getElementById("rainContainer").appendChild(drop); }')
+    example_output_2 = '{ "html": "' + example_html_2 + '", "css": "' + example_css_2 + '", "javascript": "' + example_javascript_2 + '" }'
+    
+    example_outputs = [example_output_1, example_output_2]
+
 
     messages = [
-        {'role': 'system', 'content': 'You are a website developer. Create an animation using CSS and JavaScript to show the meaning of the Japanese onomatopoeia entered by the user.'},
-        {'role': 'user', 'content': 'ザーザー'},
-        {'role': 'assistant', 'content': example_output},
-        {'role': 'user', 'content': text}
+        {'role': 'system', 'content': 'あなたは素晴らしいWebデザイナーです。'},
+        {'role': 'user', 'content': f'\
+            「{text}」を表すアニメーションを作成してください (HTML, CSS, JavaScriptを使用) \
+            - 注意: idはハッシュ関数を用いて生成し、ユニークなものにしてください \
+        '},
+        #{'role': 'user', 'content': 'ザーザー'},
+        #{'role': 'assistant', 'content': example_outputs[0]},
+        #{'role': 'user', 'content': 'ガタガタ'},
+        #{'role': 'assistant', 'content': example_outputs[1]},
     ]
 
     schema = {
         'type': 'object',
         'properties': {
-            'html': {'type': 'string'},
-            'css': {'type': 'string'},
-            'javascript': {'type': 'string'}
+            'html': {
+                'type': 'string'
+            },
+            'css': {
+                'type': 'string'
+            },
+            'javascript': {
+                'type': 'string'
+            }
         },
         'required': ['html', 'css', 'javascript'],
         'additionalProperties': False
@@ -92,11 +111,11 @@ def requst_gpt(text):
 
     openai.api_key = os.getenv('OPENAI_API_KEY')
     response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
+        model='gpt-3.5-turbo-16k',
         messages=messages,
         functions=functions,
         function_call={'name': 'set_definition'},
-        temperature=0.2,
+        temperature=0.8,
         n=1
     )
 
@@ -135,9 +154,9 @@ def format_gpt_response(res):
     print(content)
 
     # json形式の文字列を辞書型に変換
-    html_str = json.loads(content)['html']
-    css_str = json.loads(content)['css']
-    js_str = json.loads(content)['javascript']
+    html_str = json.loads(content)['html'] if json.loads(content)['html'] else ''
+    css_str = json.loads(content)['css'] if json.loads(content)['css'] else ''
+    js_str = json.loads(content)['javascript'] if json.loads(content)['javascript'] else ''
 
     response = (html_str, css_str, js_str)
     print(response)
